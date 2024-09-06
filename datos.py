f8168a276ade4c90f00343530cb2665ebe9ae5b4
life151 <fernandachamorro151@gmail.com>
2024-09-06 10:28:05 -0500

    Add files via upload

---
+++ datos.py
@@ -0,0 +1,9 @@
+productos = [
+ {'nombre': 'Celular', 'precio': 1250},
+ {'nombre': 'Computador', 'precio': 2150},
+ {'nombre': 'Tablet', 'precio': 875},
+ {'nombre': 'Consola', 'precio': 1650},
+ {'nombre': 'Lapiz', 'precio': 5},
+ {'nombre': 'Borrador', 'precio': 1.2},
+ {'nombre': 'Sacapuntas', 'precio': 2.5},
+]
---
+++ main.py
@@ -0,0 +1,17 @@
+from flask import Flask, render_template
+
+from datos import productos
+
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template('index.html', productos=productos)
+
+@app.route("/producto/<string:nombre>")
+def producto(nombre):
+    prod = [ p for p in productos if p['nombre'] == nombre ]
+    return render_template('producto.html', producto=prod[0])
+
+if __name__ == '__main__':
+  app.run(host= '0.0.0.0', port=8080, debug=True)
